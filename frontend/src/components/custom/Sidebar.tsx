126e861960039ced077bc922a247deef116ce895
Saad Sayyed <saadsyed950@gmail.com>
2026-06-16 23:56:18 +0530

    Refactored Sidebar

--- frontend/src/components/custom/Sidebar.tsx
+++ frontend/src/components/custom/Sidebar.tsx
@@ -1,5 +1,6 @@
-// src/components/Sidebar.tsx
 import clsx from "clsx";
+import { Link } from "react-router-dom";
+
 import {
   ArrowLeftRight,
   BookCheck,
@@ -11,7 +12,54 @@ import {
   TicketPercent,
   Users,
 } from "lucide-react";
-import { Link } from "react-router-dom";
+
+export const sidebarItems = [
+  {
+    text: "Dashboard",
+    link: "/",
+    icon: LayoutDashboard,
+  },
+  {
+    text: "Customers",
+    link: "/customers",
+    icon: Users,
+  },
+  {
+    text: "Products",
+    link: "/products",
+    icon: Package2,
+  },
+  {
+    text: "Discounts",
+    link: "/discounts",
+    icon: TicketPercent,
+  },
+  {
+    text: "RMA",
+    link: "/rma",
+    icon: ArrowLeftRight,
+  },
+  {
+    text: "Orders",
+    link: "/orders",
+    icon: ShoppingCart,
+  },
+  {
+    text: "Invoices",
+    link: "/invoices",
+    icon: Landmark,
+  },
+  {
+    text: "Courses",
+    link: "/courses",
+    icon: BookCheck,
+  },
+  {
+    text: "Questions",
+    link: "/tests",
+    icon: NotebookPen,
+  },
+] as const;
 
 interface SidebarProps {
   isOpen: boolean;
@@ -26,132 +74,29 @@ const Sidebar = ({ isOpen }: SidebarProps) => {
         "overflow-hidden",
       )}
     >
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <LayoutDashboard
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Dashboard
-          </span>
-          <span />
-        </Link>
-      </button>
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/customers">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <Users
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Customers
-          </span>
-          <span />
-        </Link>
-      </button>
-      {/* <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-                <Link to="/categories">
-                    <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-                        <Network size={20} className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2" />
-                        Categories
-                    </span>
-                    <span />
-                </Link>
-            </button> */}
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/products">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <Package2
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Products
-          </span>
-          <span />
-        </Link>
-      </button>
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/discounts">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <TicketPercent
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Discounts
-          </span>
-          <span />
-        </Link>
-      </button>
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/rma">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <ArrowLeftRight
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            RMA
-          </span>
-          <span />
-        </Link>
-      </button>
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/orders">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <ShoppingCart
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Orders
-          </span>
-          <span />
-        </Link>
-      </button>
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/invoices">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <Landmark
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Invoices
-          </span>
-          <span />
-        </Link>
-      </button>
-      {/* <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-                <Link to="/wishlists">
-                    <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-                        <Heart size={20} className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2" />
-                        Wishlists
-                    </span>
-                    <span />
-                </Link>
-            </button> */}
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/courses">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <BookCheck
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Courses
-          </span>
-          <span />
-        </Link>
-      </button>
-      <button className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm">
-        <Link to="/tests">
-          <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
-            <NotebookPen
-              size={20}
-              className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
-            />
-            Questions
-          </span>
-          <span />
-        </Link>
-      </button>
+      {sidebarItems.map((item) => {
+        const Icon = item.icon;
+
+        return (
+          <button
+            key={item.link}
+            className="bg-transparent text-stone-100 dark:text-stone-100 w-full shadow-none flex justify-between items-center lg:px-1.5 lg:py-2 text-base font-normal hover:bg-gray-500/30 rounded-sm"
+          >
+            <Link to={item.link}>
+              <span className="flex items-center gap-x-2 text-[15px] text-[#ABB9E8] hover:text-stone-50 transition duration-300 font-work">
+                <Icon
+                  size={20}
+                  className="min-w-[20px] min-h-[20px] stroke-[1.5] mr-2"
+                />
+
+                {item.text}
+              </span>
+
+              <span />
+            </Link>
+          </button>
+        );
+      })}
     </aside>
   );
 };
