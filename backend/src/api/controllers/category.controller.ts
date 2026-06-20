92b0f8ef22b82063893f467dac78a3f5ff38d64a
Saad Sayyed <saadsyed950@gmail.com>
2026-06-20 18:20:09 +0530

    Add category controllers: implement addCategoryController, fetchAllCategoriesController, fetchSingleCategoryController, updateCategoryController, and deleteCategoryController for comprehensive category management, including error handling for missing parameters and fields.

---
+++ backend/src/api/controllers/category.controller.ts
@@ -0,0 +1,97 @@
+import { Request, Response } from "express";
+import * as categoryServices from "../services/category.service";
+
+export const addCategoryController = async (req: Request, res: Response) => {
+  let errorMessage;
+  try {
+    const { name, imageUrl } = req.body;
+
+    const data = { name, imageUrl };
+    if (!data) {
+      errorMessage = "Required fields are missing";
+      console.log(errorMessage);
+      return res.status(400).json({ error: errorMessage });
+    }
+
+    const category = await categoryServices.addCategoryService(data);
+    res.status(201).json({ message: "Category added", category });
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+
+export const fetchAllCategoriesController = async (
+  req: Request,
+  res: Response,
+) => {
+  let errorMessage;
+  try {
+    const category = await categoryServices.fetchAllCategoriesService();
+    res.status(200).json({ total: category.length, category });
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+
+export const fetchSingleCategoryController = async (
+  req: Request,
+  res: Response,
+) => {
+  let errorMessage;
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      errorMessage = "Required param is missing";
+      console.log(errorMessage);
+      return res.status(400).json({ error: errorMessage });
+    }
+
+    const category = await categoryServices.fetchSingleCategoryService(
+      id as string,
+    );
+    res.status(200).json({ category });
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+
+export const updateCategoryController = async (req: Request, res: Response) => {
+  let errorMessage;
+  try {
+    const { id } = req.params;
+    const { name, imageUrl } = req.body;
+
+    const data = { id, name, imageUrl };
+    if (!data) {
+      errorMessage = "Required param and fields are missing";
+      console.log(errorMessage);
+      return res.status(400).json({ error: errorMessage });
+    }
+
+    const category = await categoryServices.fetchSingleCategoryService(
+      id as string,
+    );
+    res.status(200).json({ message: "Category added", category });
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+
+export const deleteCategoryController = async (req: Request, res: Response) => {
+  let errorMessage;
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      errorMessage = "Required param is missing";
+      console.log(errorMessage);
+      return res.status(400).json({ error: errorMessage });
+    }
+
+    const category = await categoryServices.deleteCategoryService(id as string);
+    res.status(204).json({ category });
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
