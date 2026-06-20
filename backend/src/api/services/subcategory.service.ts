1ab262798a9293da30cad07f801033a6ea0a5b60
Saad Sayyed <saadsyed950@gmail.com>
2026-06-20 18:12:28 +0530

    Add subcategory services: implement addSubcategoryService, fetchAllSubcategoriesService, fetchSingleSubcategoryService, updateSubcategoryService, and deleteSubcategoryService for comprehensive subcategory management, including validation for existing subcategories.

---
+++ backend/src/api/services/subcategory.service.ts
@@ -0,0 +1,58 @@
+import prisma from "../../lib/orm";
+import type { AddSubcategory, UpdateSubcategory } from "../../types/stock.type";
+
+export const addSubcategoryService = async (data: AddSubcategory) => {
+  const existing = await prisma.subcategories.findUnique({
+    where: {
+      name: data.name,
+    },
+  });
+  if (existing) throw new Error("Subcategory already exists");
+
+  const subcategory = await prisma.subcategories.create({
+    data: {
+      name: data.name,
+      imageUrl: data.imageUrl,
+      categoryId: data.categoryId,
+    },
+  });
+
+  return subcategory;
+};
+
+export const fetchAllSubcategoriesService = async () => {
+  return await prisma.subcategories.findMany();
+};
+
+export const fetchSingleSubcategoryService = async (id: string) => {
+  return await prisma.subcategories.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const updateSubcategoryService = async (data: UpdateSubcategory) => {
+  const subcategory = await prisma.subcategories.update({
+    where: {
+      id: data.id,
+    },
+    data: {
+      name: data.name,
+      imageUrl: data.imageUrl,
+      categoryId: data.categoryId,
+    },
+  });
+
+  return subcategory;
+};
+
+export const deleteSubcategoryService = async (id: string) => {
+  const subcategory = await prisma.subcategories.delete({
+    where: {
+      id,
+    },
+  });
+
+  return subcategory;
+};
