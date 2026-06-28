f691dfe4b7d0532a4793d705d681c6285c2c3043
Saad Sayyed <saadsyed950@gmail.com>
2026-06-28 15:32:52 +0530

    Add product controller

---
+++ backend/src/api/controllers/product.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import * as productServices from "../services/product.service";
+
+export const addProductController = async (req: Request, res: Response) => {
+  let errorMessage;
+  try {
+    const {
+      name,
+      description,
+      type,
+      label,
+      warranty,
+      quantity,
+      sku,
+      usps,
+      imageUrls,
+      filePath,
+      subcategoryId,
+    } = req.body;
+
+    const data = {
+      name,
+      description,
+      type,
+      label,
+      warranty,
+      quantity,
+      sku,
+      usps,
+      imageUrls,
+      filePath,
+      subcategoryId,
+    };
+    if (!data) {
+      errorMessage = "Required fields are missing";
+      console.log(errorMessage);
+      return res.status(400).json({ error: errorMessage });
+    }
+
+    const product = await productServices.addProductService(data);
+    return res.status(200).json({ product });
+  } catch (error: any) {
+    console.log(error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
