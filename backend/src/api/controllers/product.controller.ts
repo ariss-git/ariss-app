57b836e59afa5ae6990c4f3f9f7b1dbc601e610b
Saad Sayyed <saadsyed950@gmail.com>
2026-06-30 16:26:24 +0530

    Fetch single product controller

--- backend/src/api/controllers/product.controller.ts
+++ backend/src/api/controllers/product.controller.ts
@@ -68,6 +68,23 @@ export const fetchAllProductsController = async (
   }
 };
 
+export const fetchSingleProductController = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const { id } = req.params;
+
+    const product = await productServices.fetchSingleProductService(
+      id as string,
+    );
+    return res.status(200).json({ product });
+  } catch (error: any) {
+    console.log(error.message);
+    return res.status(400).json({ error: error.message });
+  }
+};
+
 export const fetchAllProductsBySubcategoryController = async (
   req: Request,
   res: Response,
