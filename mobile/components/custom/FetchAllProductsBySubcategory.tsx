a5a5dc553fcbd1c6e652578fe2e914b56501adb1
Saad Sayyed <saadsyed950@gmail.com>
2026-07-02 09:56:14 +0530

    Fetch all products by subcategory component

--- mobile/components/custom/FetchAllProductsBySubcategory.tsx
+++ mobile/components/custom/FetchAllProductsBySubcategory.tsx
@@ -1,19 +1,34 @@
-import { useState } from 'react';
+import { fetchAllProductsBySubcategoryAPI } from '@/api/product.api';
+import type { FetchAllProductsBySubcategoryType } from '@/types/product.type';
+import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 
 const FetchAllProductsAccSubcategory = () => {
-  const [products, setProducts] = useState<[]>();
+  const [products, setProducts] = useState<FetchAllProductsBySubcategoryType[]>();
+  let subcategoryId = 'b5a6a670-97f2-4d51-9708-66dcf5cf96a0';
 
   const handleFetchAllProducts = async () => {
     try {
+      await fetchAllProductsBySubcategoryAPI(subcategoryId)
+        .then((res) => {
+          console.log(res.data.products);
+          setProducts(res.data.products);
+        })
+        .catch((err) => {
+          console.error(err.data.response.error);
+        });
     } catch (error: any) {
       console.log(error.message);
     }
   };
 
+  useEffect(() => {
+    handleFetchAllProducts();
+  }, [subcategoryId]);
+
   return (
     <View>
-      <Text>FetchAllProductsBySubcategory</Text>
+      <Text>{JSON.stringify(products)}</Text>
     </View>
   );
 };
